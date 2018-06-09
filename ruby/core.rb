28d93d226ef031211a3c60780c79362c5463300f
roronya <roronya628@gmail.com>
2018-06-09 19:23:50 +0900

    cons,concat

--- ruby/core.rb
+++ ruby/core.rb
@@ -26,5 +26,7 @@ $core_ns = {
     atom?: ->(a) {Atom === a},
     deref: ->(a) {a.val},
     reset!: ->(a, b) {a.val = b},
-    swap!: ->(*a) {a[0].val = a[1][*[a[0].val].concat(a.drop(2))]}
+    swap!: ->(*a) {a[0].val = a[1][*[a[0].val].concat(a.drop(2))]},
+    cons: ->(a, b) {b.dup.unshift(a)},
+    concat: ->(a, b) {a.dup.concat(b)}
 }
